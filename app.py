5878ae7da654b252ac40e1000b76f06b0102b9bc
mr_destructive <gormeet711@gmail.com>
2022-12-29 22:45:38 +0530

    feat: jinja template render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import frontmatter
 import markdown
 import os 
+from jinja2 import Template
 from pathlib import Path
 
 post_path = Path("pages/dev-journal")
@@ -12,11 +13,14 @@ for file in onlyfiles:
     with open(Path(post_path) / file, "r") as f:
         content = f.read()
         post = frontmatter.loads(content)
-        html_content = markdown.markdown(post.content, extensions=['fenced_code','codehilite'])
+        html_content = markdown.markdown(post.content, extensions=['fenced_code',])
         if not os.path.exists(output_path):
             os.mkdir(output_path)
         default_title = datetime.today().strftime('%Y-%m-%d')
         default_title = file.split(".")[0]
-        title = str(post.get("title", default_title)) + ".html"
-        with open(Path(output_path) / title, 'w') as f:
-            f.write(html_content)
+        file_name = str(post.get("title", default_title)) + ".html"
+        with open("templates/post_template.html") as f:
+            template = Template(f.read())
+        post_html = template.render(title=default_title, content=html_content)
+        with open(Path(output_path) / file_name, 'w') as f:
+            f.write(post_html)
